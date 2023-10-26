8331fc02817932c310c8a13c37a9ee229841d697
Nikita.Semkin <Nikita.Semkin@e-mundo.de>
2023-10-26 14:47:15 +0300

    add display of notes list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Note} from './model/note.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'notiz-book';
+  notes: Note[] = [
+    {id: 1, title: 'Заметка 1', content: 'Content of Note 1'},
+    {id: 2, title: 'Заметка 2', content: 'Content of Note 2'},
+    {id: 3, title: 'Заметка 3', content: 'Content of Note 3'},
+
+  ];
+
+  onNoteSelected(selectedNote: Note) {
+
+  }
 }
--- src/app/components/note-list/note-list.component.ts
+++ src/app/components/note-list/note-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {Note} from '../../model/note.model';
+
 
 @Component({
   selector: 'app-note-list',
   templateUrl: './note-list.component.html',
   styleUrls: ['./note-list.component.css']
 })
-export class NoteListComponent implements OnInit {
+export class NoteListComponent {
+  @Input() notes: Note[];
+  @Output() noteSelected = new EventEmitter<Note>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onNoteSelect(note: Note) {
+    this.noteSelected.emit(note);
   }
-
 }
