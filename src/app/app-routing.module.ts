b2a66fe0e212a67771a05cad25503859b3874f30
Nikita.Semkin <Nikita.Semkin@e-mundo.de>
2023-10-26 14:25:21 +0300

    create header component and add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NoteListComponent} from './components/note-list/note-list.component';
+import {NoteDetailsComponent} from './components/note-details/note-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/notes', pathMatch: 'full' },
+  { path: 'notes', component: NoteListComponent },
+  { path: 'notes/:id', component: NoteDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NoteListComponent } from './components/note-list/note-list.component';
+import { NoteDetailsComponent } from './components/note-details/note-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NoteListComponent,
+    NoteDetailsComponent
   ],
   imports: [
     BrowserModule,
